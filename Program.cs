299cdee9b35adbb43e72e3c512b8f12fde16cf65
Alexander <AAAlexander2794@gmail.com>
2022-09-19 10:30:39 +0300

    Сделана попытка переписать по ООП, пока не работает, то ли авторизация слетела, то ли хрень написал в асинхроне.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp2;
 using System;
 using System.Runtime.Intrinsics.X86;
 using System.Security.Cryptography;
@@ -10,74 +11,8 @@ using TLSharp.Core;
 
 Console.WriteLine("======================================");
 
-var apiId = 11533761;
-var apiHash = "96d93a118d65971b80259ccc5b1c70fb";
+await Telegram.MyTask();
 
-var client = new TelegramClient(apiId, apiHash);
-await client.ConnectAsync();
-
-//var hash = await client.SendCodeRequestAsync("+79185351854");
-////Environment.Exit(0);
-//var code = "26513"; // you can change code in debugger
-////var t = Console.ReadLine();
-////code = t;
-//var user = await client.MakeAuthAsync("+79185351854", hash, code);
-
-//find recipient in contacts
-var result = await client.GetContactsAsync();
-var user1 = result.Users
-    .Where(x => x.GetType() == typeof(TLUser))
-    .Cast<TLUser>()
-    .Where(x => x.Username == "AnRostov");
-//if (user1.ToList().Count != 0)
-//{
-//    foreach (var u in user1)
-//    {
-//        //send message
-//        await client.SendMessageAsync(new TLInputPeerUser() { UserId = u.Id }, "test message");
-//    }
-//}
-
-var dialogs = await client.GetUserDialogsAsync() as TLDialogsSlice;
-var users = dialogs.Users.ToList();
-
-var target = new TLInputPeerUser { UserId = user1.First().Id };
-
-foreach (var dia in dialogs.Dialogs.Where(x => x.UnreadCount > 0))
-{
-    TLPeerUser peer = dia.Peer as TLPeerUser;
-    if (peer == null || peer.UserId != target.UserId) continue;
-    if (target == null) break;
-    var hist = await client.GetHistoryAsync(target, 0, -1, 0, dia.UnreadCount);
-    var some2 = (TLMessagesSlice)hist;
-    string text = "";
-    foreach (TLMessage m in some2.Messages)
-    {
-
-        
-            Console.WriteLine("{0} {1} {2}", m.Id, m.Message, m.FromId);
-        text = text + "\n" + m.Message;
-        
-    }
-    await client.SendMessageAsync(new TLInputPeerUser() { UserId = target.UserId }, "Я еще не прочитал сообщения: " + text);
-}
-
-
-    //      if (firstMessage > 0)
-    //      {
-    //          var readed = New TeleSharp.TL.Messages.TLRequestReadHistory{
-    //.Peer = target, 
-    //.Dirty = True, 
-    //.MessageId = firstMessage, 
-    //.MaxId = -1, 
-    //.ConfirmReceived = True, 
-    //.Sequence = dia.UnreadCount
-
-    //    };
-    //          var affectedMessages = await client.SendRequestAsync<TLAffectedMessages>(readed);
-    //          Thread.Sleep(5000);
-    //      }
-    //  }
 
 
 Console.WriteLine("======================================");
---
+++ Telegram.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+using TeleSharp.TL;
+using TeleSharp.TL.Messages;
+using TLSharp.Core;
+
+namespace ConsoleApp2
+{
+    internal static class Telegram
+    {
+        static int ApiId { get; }
+
+        static string ApiHash { get; }
+
+        static TelegramClient Client { get; }
+
+        static TLUser UserFrom { get; set; }
+
+        static TLUser UserTo { get; set; }
+
+        static Telegram()
+        {
+            ApiId = 11533761;
+            ApiHash = "96d93a118d65971b80259ccc5b1c70fb";
+            Client = new TelegramClient(ApiId, ApiHash);
+            ConnectAsync();
+            // *** Здесь должна происходить авторизация
+            //
+            // Присваиваем пользователей, с которыми будем работать
+            SetUsersAsync("AnRostov");
+            //MyTask().Start();
+        }
+
+        static async void ConnectAsync()
+        {
+            await Client.ConnectAsync();
+        }
+       
+        static async void AuthAsync()
+        {
+            var hash = await Client.SendCodeRequestAsync("+79185351854");
+            var code = "26513"; // you can change code in debugger
+            var t = Console.ReadLine();
+            code = t;
+            var user = await Client.MakeAuthAsync("+79185351854", hash, code);
+        }
+
+        /// <summary>
+        /// find recipient in contacts
+        /// </summary>
+        /// <param name="username"></param>
+        static async void SetUsersAsync(string username)
+        {
+            var result = await Client.GetContactsAsync();
+            var user1 = result.Users
+                .Where(x => x.GetType() == typeof(TLUser))
+                .Cast<TLUser>()
+                .Where(x => x.Username == "AnRostov").First();
+            UserFrom = user1;
+            UserTo = UserFrom;
+        }
+
+        /// <summary>
+        /// Отправляет сообщение
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="text"></param>
+        static async void SendMessageAsync(TLUser user, string text)
+        {
+            await Client.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, text);
+        }
+
+        public static async Task MyTask()
+        {
+            var dialogs = await Client.GetUserDialogsAsync() as TLDialogsSlice;
+            var users = dialogs.Users.ToList();
+            // Получать будем сообщения от UserFrom
+            var target = new TLInputPeerUser { UserId = UserFrom.Id };
+
+            foreach (var dia in dialogs.Dialogs.Where(x => x.UnreadCount >= 0))
+            {
+                TLPeerUser peer = dia.Peer as TLPeerUser;
+                if (peer == null || peer.UserId != target.UserId) continue;
+                if (target == null) break;
+                var hist = await Client.GetHistoryAsync(target, 0, -1, 0, 2);//dia.UnreadCount);
+                var some2 = (TLMessagesSlice)hist;
+                string text = "";
+                foreach (TLMessage m in some2.Messages)
+                {
+
+
+                    Console.WriteLine("{0} {1} {2}", m.Id, m.Message, m.FromId);
+                    text = text + "\n" + m.Message;
+
+                }
+                // await Client.SendMessageAsync(new TLInputPeerUser() { UserId = UserTo.Id }, "Я еще не прочитал сообщения: " + text);
+            }
+        }
+
+
+
+        //      if (firstMessage > 0)
+        //      {
+        //          var readed = New TeleSharp.TL.Messages.TLRequestReadHistory{
+        //.Peer = target, 
+        //.Dirty = True, 
+        //.MessageId = firstMessage, 
+        //.MaxId = -1, 
+        //.ConfirmReceived = True, 
+        //.Sequence = dia.UnreadCount
+
+        //    };
+        //          var affectedMessages = await client.SendRequestAsync<TLAffectedMessages>(readed);
+        //          Thread.Sleep(5000);
+        //      }
+        //  }
+
+    }
+}
